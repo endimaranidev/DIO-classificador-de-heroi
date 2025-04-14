04548f844255f9779cfb2159b926dc95701dc5d8
Endi Marani <endimarani.dev@gmail.com>
2025-04-14 10:16:17 -0300

    feat: adicionado validacao de apenas numeros para exp

--- script/script.js
+++ script/script.js
@@ -3,6 +3,7 @@ let expInput = document.querySelector(".hero-data__exp");
 let button = document.querySelector(".hero-data__button");
 let heroClass = document.querySelector(".hero-classification");
 let heroImg = document.querySelector("#hero-img");
+let spanAlert = document.querySelector('.alert')
 
 function getHeroName() {
 	return heroInput.value
@@ -18,29 +19,45 @@ button.addEventListener('click', () =>{
 
     let exp = getHeroExp()
 
-    if (exp <= 1000) {
-        heroImg.setAttribute('src', './assets/images/heroi_ferro.png')
-        heroClass.innerHTML = `${name} é do nível <span class="iron">Ferro</span>`
-    } else if (exp <= 2000) {
-        heroImg.setAttribute('src', './assets/images/heroi_bronze.jpeg')
-        heroClass.innerHTML = `${name} é do nível <span class="bronze">Bronze</span>`
-    } else if (exp <= 5000) {
-        heroImg.setAttribute('src', './assets/images/heroi_prata.jpeg')
-        heroClass.innerHTML = `${name} é do nível <span class="silver">Prata</span>`
-    } else if (exp <= 7000) {
-        heroImg.setAttribute('src', './assets/images/heroi_ouro.jpeg')
-        heroClass.innerHTML = `${name} é do nível <span class="gold">Ouro</span>`
-    } else if (exp <= 8000) {
-        heroImg.setAttribute('src', './assets/images/heroi_platina.jpeg')
-        heroClass.innerHTML = `${name} é do nível <span class="platinum">Platina</span>`
-    } else if (exp <= 9000) {
-        heroImg.setAttribute('src', './assets/images/heroi_ascendente.jpeg')
-        heroClass.innerHTML = `${name} é do nível <span class="ascending">Ascendente</span>`
-    } else if (exp <= 10000) {
-        heroImg.setAttribute('src', './assets/images/heroi_imortal.jpeg')
-        heroClass.innerHTML = `${name} é do nível <span class="imortal">Imortal</span>`
-    } else if (exp >= 10001) {
-        heroImg.setAttribute('src', './assets/images/heroi_radiante.jpeg')
-        heroClass.innerHTML = `${name} é do nível <span class="radiant">Radiante</span>`
-    } 
+    try {
+        if (name === "" || exp === "") {
+            throw new Error('Preencha todos os campos!')
+        }
+
+        exp = parseInt(exp)
+
+        if(isNaN(exp)) {
+            throw new Error ('Informe apenas números de experiência')
+        }
+        
+        spanAlert.textContent = ""
+        
+        if (exp <= 1000) {
+            heroImg.setAttribute('src', './assets/images/heroi_ferro.png')
+            heroClass.innerHTML = `${name} é do nível <span class="iron">Ferro</span>`
+        } else if (exp <= 2000) {
+            heroImg.setAttribute('src', './assets/images/heroi_bronze.jpeg')
+            heroClass.innerHTML = `${name} é do nível <span class="bronze">Bronze</span>`
+        } else if (exp <= 5000) {
+            heroImg.setAttribute('src', './assets/images/heroi_prata.jpeg')
+            heroClass.innerHTML = `${name} é do nível <span class="silver">Prata</span>`
+        } else if (exp <= 7000) {
+            heroImg.setAttribute('src', './assets/images/heroi_ouro.jpeg')
+            heroClass.innerHTML = `${name} é do nível <span class="gold">Ouro</span>`
+        } else if (exp <= 8000) {
+            heroImg.setAttribute('src', './assets/images/heroi_platina.jpeg')
+            heroClass.innerHTML = `${name} é do nível <span class="platinum">Platina</span>`
+        } else if (exp <= 9000) {
+            heroImg.setAttribute('src', './assets/images/heroi_ascendente.jpeg')
+            heroClass.innerHTML = `${name} é do nível <span class="ascending">Ascendente</span>`
+        } else if (exp <= 10000) {
+            heroImg.setAttribute('src', './assets/images/heroi_imortal.jpeg')
+            heroClass.innerHTML = `${name} é do nível <span class="imortal">Imortal</span>`
+        } else if (exp >= 10001) {
+            heroImg.setAttribute('src', './assets/images/heroi_radiante.jpeg')
+            heroClass.innerHTML = `${name} é do nível <span class="radiant">Radiante</span>`
+        }
+    } catch (error) {
+        spanAlert.textContent = error.message
+    }
 })
